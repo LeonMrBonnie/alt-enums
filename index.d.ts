25614d559b4e2a6c542562f5eb88fdcab5b730af
LeonMrBonnie <leonmrbonnie@hotmail.com>
2020-09-30 21:28:19 +0200

    Added PedFlag enum

--- index.d.ts
+++ index.d.ts
@@ -894,8 +894,46 @@ declare module "alt-enums" {
         u_m_y_zombie_01 = 0xAC4B4506
     }
 
+    export const enum PedFlag {
+        CanPunch = 18,
+        CanFlyThruWindscreen = 32,
+        DiesByRagdoll = 33,
+        PutOnMotorcycleHelmet = 35,
+        NoCollision = 52,
+        IsShooting = 58,
+        IsOnGround = 60,
+        NoCollide = 62,
+        Dead = 71,
+        IsSniperScopeActive = 72,
+        SuperDead = 73,
+        IsInAir = 76,
+        IsAiming = 78,
+        Drunk = 100,
+        IsNotRagdollAndNotPlayingAnim = 104,
+        NoPlayerMelee = 122,
+        NmMessage466 = 125,
+        InjuredLimp = 166,
+        InjuredLimp2 = 170,
+        DisableShufflingToDriverSeat = 184,
+        InjuredDown = 187,
+        Shrink = 223,
+        MeleeCombat = 224,
+        DisableStoppingVehEngine = 241,
+        IsOnStairs = 253,
+        HasOneLegOnGround = 276,
+        NoWrithe = 281,
+        Freeze = 292,
+        IsStill = 301,
+        NoPedMelee = 314,
+        PedSwitchingWeapon = 331,
+        Alpha = 410,
+        DisablePropKnockOff = 423,
+        DisableStartingVehEngine = 429,
+        FlamingFootprints = 421
+    }
+
     // For e.g. disableControlAction native
-    export enum InputGroup {
+    export const enum InputGroup {
         Move = 0,
         Look,
         Wheel,
